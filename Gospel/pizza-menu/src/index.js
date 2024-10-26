4751fa9680abccf0c7c888d27adceb2cb19ffba2
Gospel Ugochukwu <Ugochukwu Gospel>
2024-10-26 15:42:45 +0100

    started pizza-menu project

---
+++ Gospel/pizza-menu/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React!</h1>
+    </div>
+  );
+}
+
+
+
+
+
+// Render react app
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
