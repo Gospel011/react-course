9771af219ef91753a76249c51b92f92baf0a06ee
Gospel Ugochukwu <gospelugochukwu@Gospels-MacBook-Pro.local>
2025-01-28 22:03:31 +0100

    saved

--- Gospel/the-wild-oasis/vite.config.js
+++ Gospel/the-wild-oasis/vite.config.js
@@ -5,4 +5,7 @@ import eslint from "eslint-plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), eslint],
+  server: {
+    allowedHosts: ["74bc-2c0f-2a80-9-8510-c146-aa2-37b0-803.ngrok-free.app"],
+  },
 });
